cd9724fe4ec61aa053124e9f3048e83e5660d3b9
wangchaozhuanyong <ppfzj1314@gmail.com>
2026-08-08 02:40:44 -0700

    refactor(v3): switch cart to final unified commerce page

--- apps/storefront/app/preview/v3/[locale]/cart/page.tsx
+++ apps/storefront/app/preview/v3/[locale]/cart/page.tsx
@@ -1,9 +1,9 @@
 import { notFound } from "next/navigation";
-import { V3CommerceCartPage } from "../../../../../components/v3-preview/v3-commerce-layer";
+import { V3CartFinal } from "../../../../../components/v3-preview/v3-final-pages";
 import { isLocale } from "../../../../../lib/copy";
 
 export default async function Page({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
   if (!isLocale(locale)) notFound();
-  return <V3CommerceCartPage locale={locale} />;
+  return <V3CartFinal locale={locale} />;
 }
