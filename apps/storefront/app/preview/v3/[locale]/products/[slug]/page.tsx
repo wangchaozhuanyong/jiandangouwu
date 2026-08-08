e9f380c7bed6ac61b819bd73218a428d2fa93ab8
wangchaozhuanyong <ppfzj1314@gmail.com>
2026-08-08 02:40:11 -0700

    refactor(v3): switch product detail to final commerce page

--- apps/storefront/app/preview/v3/[locale]/products/[slug]/page.tsx
+++ apps/storefront/app/preview/v3/[locale]/products/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import { notFound } from "next/navigation";
-import { V3ProductDetail } from "../../../../../../components/v3-preview/v3-pages";
+import { V3ProductDetailFinal } from "../../../../../../components/v3-preview/v3-final-pages";
 import { isLocale } from "../../../../../../lib/copy";
 
 export default async function Page({ params }: { params: Promise<{ locale: string; slug: string }> }) {
   const { locale, slug } = await params;
   if (!isLocale(locale)) notFound();
-  return <V3ProductDetail locale={locale} slug={slug} />;
+  return <V3ProductDetailFinal locale={locale} slug={slug} />;
 }
